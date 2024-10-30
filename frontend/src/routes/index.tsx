e6729eac7e81d789d9719340e22497e1bdad5d7d
Nick <ko.binghong@gmail.com>
2024-10-30 11:17:29 -0400

    changes in frontend/src/index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <div>Phase 2 - A Trustworthy Module Registry</div>,
+  component: () => <div>Phase 2 - A Trustworthy Module Registry: Yay! thanks guys let's keep going 🔥</div>,
 });

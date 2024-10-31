62dd5fb9973c9e9f0c6d302337c632cc1f4b90fd
Gaurav V <120694552+gv912@users.noreply.github.com>
2024-10-31 19:50:18 -0400

    Update index.tsx

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <div>
-    <h1></h1>Phase 2 - A Trustworthy Module Registry </h1>
+  component: () => (
+    <div>
+    <h1>Phase 2 - A Trustworthy Module Registry </h1>
     <p> Thanks to all Team 8 Contributors 🙏 </p>
-  </div>,
+  </div>),
 });

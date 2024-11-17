e164b44c7d1957e8c9a0e521fe5833107e033e36
Jimmy Ho <90421694+Jimmy-Hcc@users.noreply.github.com>
2024-11-17 10:46:42 -0500

    push complete url upload

--- frontend/src/routes/upload.tsx
+++ frontend/src/routes/upload.tsx
@@ -39,7 +39,8 @@ function uploadPackage() {
       if (!res.ok) {
         throw new Error(`Error uploading package: ${res.statusText}`);
       }
-
+      
+      //navigate to the package page after successful upload
       navigate({ to: "/package" });
     },
   });

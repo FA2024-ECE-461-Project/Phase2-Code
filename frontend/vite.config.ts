804bee2c1ebce257972543ad6694070e5471d4a7
Jimmy Ho <90421694+Jimmy-Hcc@users.noreply.github.com>
2024-11-17 18:37:03 -0500

    change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
 import path from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 // import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+// Resolve __dirname in ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dir, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {

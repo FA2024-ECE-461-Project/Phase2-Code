848afc9f6092c4195401a4d78454cfbaf25e4600
Jimmy Ho <90421694+Jimmy-Hcc@users.noreply.github.com>
2024-11-18 18:14:29 -0500

    delete node_modeul and lock.json for cd

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
     dialect: "postgresql",
     schema: "./server/db/schemas/*",
     dbCredentials: {
-      host: "trustworthy-registry-db.cb04wqeiekix.us-east-2.rds.amazonaws.com",
-      database: "postgres",
-      user: "team8",
-      password: "ECE461.team8.aws",
+      host: process.env.DATABASE_HOST!,
+      database: process.env.DATABASE_NAME!,
+      user: process.env.DATABASE_USER!,
+      password: process.env.DATABASE_PASSWORD!,
       port: 5432,
       ssl: {
         rejectUnauthorized: false, // Temporarily bypass SSL validation
@@ -15,4 +15,3 @@ export default defineConfig({
     },
   });
   
-// psql "host=trustworthy-registry-db.cb04wqeiekix.us-east-2.rds.amazonaws.com port=5432 dbname=postgres user=team8 password=ECE461.team8.aws sslmode=require"

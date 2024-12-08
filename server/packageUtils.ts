52075b37b923e2b6167248abcbf89282c45dbde1
gv912 <gvermani@purdue.edu>
2024-12-08 12:46:54 -0500

    fixed url in db

--- server/packageUtils.ts
+++ server/packageUtils.ts
@@ -116,7 +116,7 @@ export async function uploadToS3viaBuffer(buffer: Buffer, key: string, contentTy
 }
 
 
-export function extractMetadataFromZip(buffer: Buffer): { Name: string; Version: string } {
+export function extractMetadataFromZip(buffer: Buffer): { Name: string; Version: string, URL: string } {
   const zip = new AdmZip(buffer);
   const zipEntries = zip.getEntries();
 
@@ -135,7 +135,7 @@ export function extractMetadataFromZip(buffer: Buffer): { Name: string; Version:
 
   const packageJsonStr = packageJsonEntry.getData().toString('utf-8');
 
-  let packageJson: { name?: string; version?: string; url?: string };
+  let packageJson: { name?: string; version?: string; repository?: {url?: string} };
   try {
     packageJson = JSON.parse(packageJsonStr);
   } catch (error) {
@@ -144,8 +144,9 @@ export function extractMetadataFromZip(buffer: Buffer): { Name: string; Version:
 
   const Name = packageJson.name || 'Default-Name';
   const Version = packageJson.version || '1.0.0';
+  const URL = packageJson.repository?.url || "";
 
-  return { Name, Version };
+  return { Name, Version, URL };
 }
 
 export function removeDotGitFolderFromZip(buffer: Buffer): string {
